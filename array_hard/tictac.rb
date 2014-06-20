8796af5c8bb73ca594ee0f8dd8031735104f6ff5
Adam Legg <adam.legg@gmail.com>
2014-06-20 13:18:16 -0600

    tictac occurences

---
+++ array_hard/tictac.rb
@@ -0,0 +1,13 @@
+array = [['X', 'O', 'O'],['X', 'X', 'O'], ['O', 'X', 'O']]
+
+
+puts "Below you find the number of O's and X's in each row:"
+puts "O's:"
+puts array[0].count "O"
+puts array[1].count "O"
+puts array[2].count "O"
+puts "X's:"
+puts array[0].count "X"
+puts array[1].count "X"
+puts array[2].count "X"
+
